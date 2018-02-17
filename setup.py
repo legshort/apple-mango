f9dc5c03983b4cebbd6bcd64fe70bd536936ed18
Jun Young Lee <legshort@gmail.com>
2018-02-17 17:47:35 +0900

    release(setup): update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name='apple-mango',
-    version='0.1',
+    version='0.2',
     url='https://github.com/legshort/apple-mango/',
     license='MIT',
     author='Jun Young Lee',
